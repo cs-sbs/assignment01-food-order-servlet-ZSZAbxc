24f51c4d45cd8cec294e551722110ed56cebd108
ZSZAbxc <110092724+ZSZAbxc@users.noreply.github.com>
2026-06-04 22:18:04 +0800

    Update Order.java

--- src/main/java/cs/sbs/web/model/Order.java
+++ src/main/java/cs/sbs/web/model/Order.java
@@ -3,8 +3,34 @@ package cs.sbs.web.model;
 public class Order {
 
     private int id;
-    private String customer;
-    private String food;
-    private int quantity;
+    private final String customer;
+    private final String food;
+    private final int quantity;
 
+    public Order(int id, String customer, String food, int quantity) {
+        this.id = id;
+        this.customer = customer;
+        this.food = food;
+        this.quantity = quantity;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCustomer() {
+        return customer;
+    }
+
+    public String getFood() {
+        return food;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
 }
